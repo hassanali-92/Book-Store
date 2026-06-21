4e843d0f1244407e4a2ac0561eb4e7e272ba0b46
hassanali-92 <hassan.programmer92@gmail.com>
2026-06-22 00:47:47 +0500

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 // ✅ Production-Ready CORS Configuration
 app.use(cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173', // Frontend URL
+    origin: process.env.CLIENT_URL || 'https://book-store-frontend-phi-ten.vercel.app/', // Frontend URL
     credentials: true,           // Cookies/Auth headers allow karo
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Sirf zaroori methods
     allowedHeaders: ['Content-Type', 'Authorization'] // Sirf zaroori headers
